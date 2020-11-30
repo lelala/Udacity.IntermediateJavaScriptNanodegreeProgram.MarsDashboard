4a40cbc06d4daba35e58bd239e44cf8d629b90c0
Lelala <lelala@ischool.com.tw>
2020-11-30 14:47:54 +0800

    refactor client script, make it simplier

--- src/public/client.js
+++ src/public/client.js
@@ -36,182 +36,165 @@
 //         }
 //     ]
 // }
+
+// status object
 let marsDashboard = {
     loading: true
     , rover: []
 }
 
+// draw/redraw whole html body
+const draw = (() => {
+    let checkData;
+    return (newData) => {
+        // check if data really changed
+        {
+            if (Immutable.Map({}).mergeDeep(newData).equals(checkData))
+                return;
+            checkData = Immutable.Map({}).mergeDeep(newData);
+        }
+        // draw 
+        {
+            const currentRover = newData.rover.find(roverObj => roverObj.isActive) || {};
+            document.querySelector("body").innerHTML = `
+    <div class="container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="">
+            <div class="clearfix">
+                <h3 class="float-md-left text-white">Mars Dashboard</h3>
+                ${newData.loading ? `` : `
+                <ul class="nav float-md-right justify-content-center">
+                    ${["", ...newData.rover].reduce((html, roverObj, index) => html += `
+                    <li class="nav-item ">
+                        <a class="nav-link text-light pb-1 ${roverObj.isActive ? "border-bottom border-light" : ""}" 
+                            onclick="setRover(${index - 1})"
+                            href="javascript:void(0)">
+                            ${roverObj.name}
+                        </a>
+                    </li>
+                    `)}
+                </ul>
+                `}
+            </div>
+            ${newData.loading ? `` : `
+            <section class="jumbotron py-4">
+                <h4 class="text-center">${currentRover.name}</h4>
+                <p class="text-left text-muted">${currentRover.description}</p>
+            </section>
+            `}
+        </header>
+        ${newData.loading ? `
+        <div class="d-flex justify-content-center my-auto">
+            <div class="spinner-border text-light" role="status">
+            <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        ` : `
+        <main role="main"
+                class="row">
+            ${["", ...currentRover.camera || []].reduce((html, cameraObj, index) => html += `
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card bg-secondary text-light mb-4 mb-sm-5 shadow-sm">
+                    <div class="carousel slide card-img-top"
+                        data-ride="carousel">
+                        <div class="carousel-inner">
+                        ${["", ...cameraObj.photo].reduce((html, photo, index) => html += `
+                            <div class="carousel-item ${index == 1 ? "active" : ""}">
+                                <img src="${photo}"
+                                    class="d-block w-100"
+                                    alt="..."
+                                    style="height: 225px;">
+                                <a href="${photo}"
+                                    target="_blank"
+                                    class="text-white"
+                                    style="position: absolute; right:5px; bottom:5px;z-index:2;">
+                                    <svg width="1em"
+                                        height="1em"
+                                        viewBox="0 0 16 16"
+                                        class="bi bi-box-arrow-in-up-right"
+                                        fill="currentColor"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                            d="M6.364 13.5a.5.5 0 0 0 .5.5H13.5a1.5 1.5 0 0 0 1.5-1.5v-10A1.5 1.5 0 0 0 13.5 1h-10A1.5 1.5 0 0 0 2 2.5v6.636a.5.5 0 1 0 1 0V2.5a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5H6.864a.5.5 0 0 0-.5.5z" />
+                                        <path fill-rule="evenodd"
+                                            d="M11 5.5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793l-8.147 8.146a.5.5 0 0 0 .708.708L10 6.707V10.5a.5.5 0 0 0 1 0v-5z" />
+                                    </svg>
+                                </a>
+                            </div>
+                        `)}
+                        </div>
+                        <a class="carousel-control-prev"
+                            role="button"
+                            data-slide="prev">
+                            <span class="carousel-control-prev-icon"
+                                    aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next"
+                            role="button"
+                            data-slide="next">
+                            <span class="carousel-control-next-icon"
+                                    aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">${cameraObj.full_name}</p>
+                    </div>
+                </div>
+            </div>
+            `)}
+        </main>`}
+        <footer class="mt-auto text-white">
+            <p>Power by
+                <a class="text-white"
+                    href="https://api.nasa.gov/"
+                    target="_blank">NASA
+                </a>.
+            </p>
+        </footer>
+    </div>
+            `;
+        }
+        // active bootstrap carousel
+        {
+            $('.carousel').each((index, carouselObj) => {
+                carouselObj = $(carouselObj);
+                carouselObj.carousel({
+                    interval: 3000
+                    , pause: false
+                });
+                carouselObj.find('.carousel-control-prev').click(() => {
+                    carouselObj.carousel('prev')
+                });
+                carouselObj.find('.carousel-control-next').click(() => {
+                    carouselObj.carousel('next')
+                });
+            });
+        }
+    };
+})();
+
+// change current rover
 const setRover = (index) => {
     marsDashboard.rover.forEach((roverObj, i) => roverObj.isActive = index == i);
     draw(marsDashboard);
 };
 
-const draw = (() => {
-    // check if data really changed
-    let checkData;
-    return (newData) => {
-        if (Immutable.Map({}).mergeDeep(newData).equals(checkData))
-            return;
-        checkData = Immutable.Map({}).mergeDeep(newData);
-        if(newData.loading){
-            // draw loading
-            document.querySelector("body").innerHTML = `
-        <div class="container d-flex w-100 h-100 p-3 mx-auto flex-column">
-            <header class="">
-                <div class="clearfix">
-                    <h3 class="float-md-left text-white">Mars Dashboard</h3>
-                    <ul class="nav float-md-right justify-content-center">
-                        ${["", ...newData.rover].reduce((html, roverObj, index) => html += `
-                        <li class="nav-item ">
-                            <a class="nav-link text-light pb-1 ${roverObj.isActive ? "border-bottom border-light" : ""}" 
-                               onclick="setRover(${index - 1})"
-                               href="javascript:void(0)">
-                                ${roverObj.name}
-                            </a>
-                        </li>
-                        `)}
-                    </ul>
-                </div>
-            </header>
-            <div class="d-flex justify-content-center my-auto">
-                <div class="spinner-border text-light" role="status">
-                <span class="sr-only">Loading...</span>
-                </div>
-            </div>
-            <footer class="text-white">
-                <p>Power by
-                    <a class="text-white"
-                        href="https://api.nasa.gov/"
-                        target="_blank">NASA
-                    </a>.
-                </p>
-            </footer>
-        </div>
-            `;
-        }
-        else{
-            // draw 
-            const currentRover = newData.rover.find(roverObj => roverObj.isActive);
-            if (currentRover) {
-                document.querySelector("body").innerHTML = `
-        <div class="container d-flex w-100 h-100 p-3 mx-auto flex-column">
-            <header class="">
-                <div class="clearfix">
-                    <h3 class="float-md-left text-white">Mars Dashboard</h3>
-                    <ul class="nav float-md-right justify-content-center">
-                        ${["", ...newData.rover].reduce((html, roverObj, index) => html += `
-                        <li class="nav-item ">
-                            <a class="nav-link text-light pb-1 ${roverObj.isActive ? "border-bottom border-light" : ""}" 
-                               onclick="setRover(${index - 1})"
-                               href="javascript:void(0)">
-                                ${roverObj.name}
-                            </a>
-                        </li>
-                        `)}
-                    </ul>
-                </div>
-                <section class="jumbotron py-4">
-                    <h4 class="text-center">${currentRover.name}</h4>
-                    <p class="text-left text-muted">${currentRover.description}</p>
-                </section>
-            </header>
-    
-            <main role="main"
-                  class="row">
-                ${["", ...currentRover.camera].reduce((html, cameraObj, index) => html += `
-                <div class="col-12 col-md-6 col-lg-4">
-                    <div class="card bg-secondary text-light mb-4 mb-sm-5 shadow-sm">
-                        <div class="carousel slide card-img-top"
-                            data-ride="carousel">
-                            <div class="carousel-inner">
-                            ${["", ...cameraObj.photo].reduce((html, photo, index) => html += `
-                                <div class="carousel-item ${index == 1 ? "active" : ""}">
-                                    <img src="${photo}"
-                                        class="d-block w-100"
-                                        alt="..."
-                                        style="height: 225px;">
-                                    <a href="${photo}"
-                                        target="_blank"
-                                        class="text-white"
-                                        style="position: absolute; right:5px; bottom:5px;z-index:2;">
-                                        <svg width="1em"
-                                            height="1em"
-                                            viewBox="0 0 16 16"
-                                            class="bi bi-box-arrow-in-up-right"
-                                            fill="currentColor"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M6.364 13.5a.5.5 0 0 0 .5.5H13.5a1.5 1.5 0 0 0 1.5-1.5v-10A1.5 1.5 0 0 0 13.5 1h-10A1.5 1.5 0 0 0 2 2.5v6.636a.5.5 0 1 0 1 0V2.5a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5H6.864a.5.5 0 0 0-.5.5z" />
-                                            <path fill-rule="evenodd"
-                                                d="M11 5.5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793l-8.147 8.146a.5.5 0 0 0 .708.708L10 6.707V10.5a.5.5 0 0 0 1 0v-5z" />
-                                        </svg>
-                                    </a>
-                                </div>
-                            `)}
-                            </div>
-                            <a class="carousel-control-prev"
-                                role="button"
-                                data-slide="prev">
-                                <span class="carousel-control-prev-icon"
-                                        aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next"
-                                role="button"
-                                data-slide="next">
-                                <span class="carousel-control-next-icon"
-                                        aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                        <div class="card-body">
-                            <p class="card-text">${cameraObj.full_name}</p>
-                        </div>
-                    </div>
-                </div>
-                `)}
-            </main>
-    
-            <footer class="mt-auto text-white">
-                <p>Power by
-                    <a class="text-white"
-                        href="https://api.nasa.gov/"
-                        target="_blank">NASA
-                    </a>.
-                </p>
-            </footer>
-        </div>
-                `;
-    
-                // active bootstrap carousel
-                // $('.carousel').carousel();
-                $('.carousel').each((index, carouselObj) => {
-                    carouselObj = $(carouselObj);
-                    carouselObj.carousel({
-                        interval: 3000
-                        , pause: false
-                    });
-                    carouselObj.find('.carousel-control-prev').click(() => {
-                        carouselObj.carousel('prev')
-                    });
-                    carouselObj.find('.carousel-control-next').click(() => {
-                        carouselObj.carousel('next')
-                    });
-                });
-            }
-            else{
-                setRover(0);
-            }
-        }
-    };
-})();
+//pack load event
+const onload = (callback) => {
+    window.addEventListener('load', callback);
+};
 
-window.addEventListener('load', () => {
+//startup app
+onload(() => {
     draw(marsDashboard);
     fetch(`/api/mars_dashboard`)
         .then(res => res.json())
         .then(respObj => {
             Object.assign(marsDashboard, { loading: false }, respObj);
+            marsDashboard.rover.forEach((roverObj, index) => {
+                roverObj.isActive = index == 0;
+            });
             draw(marsDashboard);
         });
 });
